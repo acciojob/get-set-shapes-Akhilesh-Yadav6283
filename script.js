eceb2167fc988f3a3e7fd157cc06dabeeabca245
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2024-01-18 11:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-//complete this code
 class Rectangle {
-	constructor(width, height){
-	this._width = width;
-	this._height = height;
-	}
-	get width(){
-		return this._width;
-	}
-	get height(){
-		return this._height;
-	}
-	getArea(){
-		this._area = this._width*this._height;
-		return this._area;
-	}
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
 }
 
 class Square extends Rectangle {
-	getPerimeter(){
-		this._getPerimeter = 4*this._width;
-		return this._getPerimeter;
-	}
+    getPerimeter() {
+        return 4 * this.width; // Use the 'width' property from the superclass
+    }
 }
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+// Example usage in your test
+// const rectangle = new Rectangle(2, 3);
+// console.log(rectangle.getArea()); // Output: 6
+
+// const square = new Square(2);
+// console.log(square.getPerimeter()); // Output: 8
